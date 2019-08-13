44f6ff238f9d63960fe520f330e5158fc4ec8dca
Clayton Wang <claydwang@gmail.com>
2019-08-13 12:13:47 -0700

    finish the win algo

--- practice.py
+++ practice.py
@@ -40,30 +40,35 @@ class Board:
   def switch(self):
     if self.current_player == self.player1:
       self.current_player = self.player2
-    else:
+    elif self.current_player == self.player2:
       self.current_player = self.player1
 
   def horizontal(self):
     for row in self.board:
-       if row[0] == row[1] and row[0] == row[2]:
+       if row[0] == self.current_player and row[1] == self.current_player and row[2] == self.current_player:
         print(self.current_player + " is the winner")
         self.current_player = ""
         break
 
   def vertical(self):
     for i in range(len(self.board)):
-      if self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i]:
+      if self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i] and self.board[0][i] == self.current_player:
         print(self.current_player + " is the winner")
         self.current_player = ""
         break
   
   def dia(self):
-    if (self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2]):
+    if (self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2]) and self.board[0][0] == self.current_player:
       print(self.current_player + " is the winner")
       self.current_player = ""
-    if (self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0]):
+      return 
+    if (self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0]) and self.board[0][2] == self.current_player:
       print(self.current_player + " is the winner")
       self.current_player = ""
+      return
+
+
+
 
 
 game = Board()
